85ecb580042a3b1b5ab84f9114323032dd9c12ff
Charlone <charlone_91@hotmail.com>
2022-05-21 13:55:41 +0200

    add movies reducer to root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import apiCallsInProgress from "./apiStatusReducer"
 import viewRequested from "./viewRequestedReducer";
+import moviesReducer from "./moviesReducer";
 
 const rootReducer = combineReducers({
     apiCallsInProgress,
-    viewRequested
+    viewRequested,
+    moviesReducer,
 });
 
 export default rootReducer;

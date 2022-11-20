657902781f41fc3a0c821aa5f215b8aa5d643218
Charlone Agius <charlone_91@hotmail.com>
2022-11-20 12:32:27 +0100

    add link to footer and style

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,7 +4,7 @@ const Footer = ({isLoading}) => {
     return (
         <footer className={isLoading ? 'footer-container d-none' : "footer-container"} style={{top: document.body.outerHeight}}>
             <div className={"footer"}>
-                <span>Developed by <strong>Charlone Agius</strong></span>
+                <span>Developed by <strong><a href={"https://charlone-portfolio.vercel.app"} target={"_blank"} rel={"nofollow"}>Charlone Agius</a></strong></span>
             </div>
         </footer>
     )
